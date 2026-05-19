5ce3ca6216a37deef219d3f14f5da2697e974351
bansos567 <notopribadi25@gmail.com>
2026-05-20 02:25:09 +0700

    Update CustomSplash.java

--- src/com/bos/customsplash/CustomSplash.java
+++ src/com/bos/customsplash/CustomSplash.java
@@ -6,29 +6,22 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
 
-// Import Lottie
-import com.airbnb.lottie.LottieAnimationView;
-
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.SimpleFunction;
 import com.google.appinventor.components.annotations.SimpleObject;
-import com.google.appinventor.components.annotations.UsesLibraries;
 import com.google.appinventor.components.runtime.AndroidNonvisibleComponent;
 import com.google.appinventor.components.runtime.AndroidViewComponent;
 import com.google.appinventor.components.runtime.ComponentContainer;
 
 @DesignerComponent(version = 1,
-        description = "Custom Splash Screen Extension with HTML & Lottie support",
+        description = "Custom Splash Screen Extension with HTML support",
         iconName = "images/extension.png",
         nonVisible = true)
 @SimpleObject(external = true)
-// PERHATIAN: Di sistem Ant, library eksternal wajib dideklarasikan di sini
-@UsesLibraries(libraries = "lottie.aar, lottie.jar") 
 public class CustomSplash extends AndroidNonvisibleComponent {
 
     private Context context;
     private WebView webView;
-    private LottieAnimationView lottieView;
     private ViewGroup containerView;
 
     public CustomSplash(ComponentContainer container) {
@@ -36,7 +29,7 @@ public class CustomSplash extends AndroidNonvisibleComponent {
         this.context = container.$context();
     }
 
-    // 1. FUNGSI UNTUK HTML
+    // FUNGSI UNTUK HTML
     @SimpleFunction(description = "Initialize Splash Screen with HTML content")
     public void InitializeHTMLSplash(AndroidViewComponent container, String htmlContent) {
         View view = container.getView();
@@ -56,29 +49,7 @@ public class CustomSplash extends AndroidNonvisibleComponent {
         }
     }
 
-    // 2. FUNGSI UNTUK LOTTIE
-    @SimpleFunction(description = "Initialize Splash Screen with Native Lottie Animation")
-    public void InitializeLottieSplash(AndroidViewComponent container, String lottieFileName) {
-        View view = container.getView();
-        if (view instanceof ViewGroup) {
-            this.containerView = (ViewGroup) view;
-            
-            lottieView = new LottieAnimationView(context);
-            lottieView.setLayoutParams(new FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT));
-            
-            // Render dari file JSON yang ada di Assets
-            lottieView.setAnimation(lottieFileName);
-            lottieView.playAnimation();
-            lottieView.loop(true);
-            
-            this.containerView.addView(lottieView);
-            this.containerView.setVisibility(View.VISIBLE);
-        }
-    }
-
-    // 3. FUNGSI HIDE & CLEAR RAM
+    // FUNGSI HIDE & CLEAR RAM
     @SimpleFunction(description = "Hide the Splash Screen Container")
     public void HideSplash() {
         if (this.containerView != null) {
@@ -87,11 +58,6 @@ public class CustomSplash extends AndroidNonvisibleComponent {
             
             if (this.webView != null) {
                 this.webView = null;
-            }
-            
-            if (this.lottieView != null) {
-                this.lottieView.cancelAnimation();
-                this.lottieView = null;
             }
         }
     }
